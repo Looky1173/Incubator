2661a6602e16df083a0c39045ee4610e97cac4c7
Looky1173 <lgl1173and2006@gmail.com>
2022-04-04 08:52:21 +0200

    Add Umami Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { UIProvider } from '@design-system/theme';
 import { createTheme } from '@design-system/theme/stitches.config';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import Head from 'next/head';
 
 import { CustomToastProvider, ToastContainer } from '@design-system';
 
@@ -17,30 +18,35 @@ const darkTheme = createTheme({
 
 function MyApp({ Component, pageProps }) {
     return (
-        <SWRConfig
-            value={{
-                fetcher: fetchJson,
-                onError: (err) => {
-                    console.error(err);
-                },
-            }}
-        >
-            <NextThemesProvider
-                defaultTheme="system"
-                attribute="class"
+        <>
+            <Head>
+                <script async defer data-website-id="77abe5c5-6a80-46c5-a574-44eeb19fd6de" src="https://umami-production-dc41.up.railway.app/umami.js" />
+            </Head>
+            <SWRConfig
                 value={{
-                    light: lightTheme.className,
-                    dark: darkTheme.className,
+                    fetcher: fetchJson,
+                    onError: (err) => {
+                        console.error(err);
+                    },
                 }}
             >
-                <UIProvider>
-                    <CustomToastProvider>
-                        <Component {...pageProps} />
-                        <ToastContainer />
-                    </CustomToastProvider>
-                </UIProvider>
-            </NextThemesProvider>
-        </SWRConfig>
+                <NextThemesProvider
+                    defaultTheme="system"
+                    attribute="class"
+                    value={{
+                        light: lightTheme.className,
+                        dark: darkTheme.className,
+                    }}
+                >
+                    <UIProvider>
+                        <CustomToastProvider>
+                            <Component {...pageProps} />
+                            <ToastContainer />
+                        </CustomToastProvider>
+                    </UIProvider>
+                </NextThemesProvider>
+            </SWRConfig>
+        </>
     );
 }
 
