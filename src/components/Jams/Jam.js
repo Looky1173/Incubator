fdc536b15b9ec75f74a28eb3168ea42cc3a9ade0
Looky1173 <lgl1173and2006@gmail.com>
2022-06-12 20:45:39 +0200

    Scratch game jam image loading fixes

--- src/components/Jams/Jam.js
+++ src/components/Jams/Jam.js
@@ -1,20 +1,30 @@
 import { Box, Card, Flex, Heading, Text, Skeleton, theme, styled } from '@design-system';
 import { imageDomains } from '@constants';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-function ErrorFallback({ error }) {
-    console.error(error);
+function ErrorFallback({ error = undefined }) {
     return (
         <Flex align="center" justify="center" direction="column" css={{ backgroundColor: '$danger3', color: '$danger11', width: '100%', aspectRatio: '16 / 9', px: '$4', btlr: '$4', btrr: '$4' }}>
             <Heading as="h2" css={{ color: 'inherit' }}>
                 This image could not be loaded!
             </Heading>
             <br />
-            <Text css={{ color: 'inherit', textAlign: 'center' }}>
-                Images are only accepted from <i>assets.scratch.mit.edu</i>! Check the console for details.
-            </Text>
+            {error && <Text css={{ color: 'inherit', textAlign: 'center' }}>{error}</Text>}
         </Flex>
     );
+}
+
+function isValidHttpUrl(string) {
+    console.log('Str', string);
+    let url;
+
+    try {
+        url = new URL(string);
+    } catch (_) {
+        return false;
+    }
+
+    return url.protocol === 'http:' || url.protocol === 'https:';
 }
 
 const StyledThumbnail = styled('img', {
@@ -24,21 +34,35 @@ const StyledThumbnail = styled('img', {
     aspectRatio: '16 / 9',
 });
 
-function Thumbnail({ image = 'https://assets.scratch.mit.edu', loading }) {
+function Thumbnail({ image, loading }) {
     const [loadingThumbnail, setLoadingThumbnail] = useState(true);
-    const { hostname } = new URL(image);
+    const hostname = isValidHttpUrl(image) === false ? null : new URL(image).hostname;
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        if (image === undefined) return;
+        setError(
+            isValidHttpUrl(image) === false ? (
+                <>Malformed image URL</>
+            ) : imageDomains.includes(hostname) ? (
+                false
+            ) : (
+                <>
+                    Images are only accepted from <i>assets.scratch.mit.edu</i>! This image is hosted under the domain <i>{hostname}</i>
+                </>
+            ),
+        );
+    }, [image, loading]);
 
     return (
         <>
-            {(loadingThumbnail === true || loading === true) && imageDomains.includes(hostname) && (
-                <Skeleton width="100%" aspectRatio="16 / 9" borderRadius={`${theme.radii[4]} ${theme.radii[4]} 0 0`} />
-            )}
-            {imageDomains.includes(hostname) ? (
+            {(loadingThumbnail === true || loading === true) && error === false && <Skeleton width="100%" aspectRatio="16 / 9" borderRadius={`${theme.radii[4]} ${theme.radii[4]} 0 0`} />}
+            {imageDomains.includes(hostname) && error === false ? (
                 <Box css={{ display: loadingThumbnail === true ? 'none' : 'auto' }}>
                     <StyledThumbnail src={image} onLoad={() => setLoadingThumbnail(false)} />
                 </Box>
             ) : (
-                <>{loading === false && <ErrorFallback />}</>
+                <>{loading === false && error !== false && <ErrorFallback error={error} />}</>
             )}
         </>
     );
