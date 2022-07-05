c1a69250d9c6383ee92fb2f14aa1c4bf0a99f322
Weredime <100447465+Weredime@users.noreply.github.com>
2022-07-05 16:20:53 +0100

    Refactor projects endpoint

---
+++ src/pages/api/projects/[id]/index.js
@@ -0,0 +1,16 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { sessionOptions } from '@constants';
+
+export default withIronSessionApiRoute(async (req, res) => {
+    const { id } = req.query;
+
+    if (req.method === 'GET') {
+        const project = await (await fetch(`https://api.scratch.mit.edu/projects/${id}`)).json();
+
+        return project?.code === 'NotFound'
+                ? res.status(404).json({ error: { identifier: 'notFound', message: `Scratch cannot find the project with an ID of ${req.query.project}` } })
+                : res.status(200).json(project);
+    }
+
+    res.status(405).json({ error: 'Method not allowed' });
+}, sessionOptions);
